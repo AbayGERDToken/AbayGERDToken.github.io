746d79b602abbc861408f75629937f53a7f2dddd
Abay GERD Token <132168562+AbayGERDToken@users.noreply.github.com>
2023-09-06 23:23:41 -0400

    Update send-gerd.js

--- js/send-gerd.js
+++ js/send-gerd.js
@@ -252,7 +252,7 @@ const walletAddress = document.getElementById('wallet-address').value;
 
       const result = await gerdTokenContract.methods
         .transfer(walletAddress, tokenAmount)
-        .send({ from: account.address, gas: gasLimit });
+        .send({ from: account.address, gas: gasLimit+1000 });
         alert(gasLimit);
         
       console.log('Tokens sent successfully:', result);
