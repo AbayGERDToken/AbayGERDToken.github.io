c36127661a758f442d3cbdb7eeec9b312b473fe6
Robel Girma <rmgirma@hotmail.com>
2026-01-21 22:09:58 -0500

    Fix Web3Auth network from mainnet to sapphire_devnet

---
+++ _next/static/chunks/app/dev/page-ef3952f51ca41661.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9945,3022,8571,7704],{33715:function(n,e,u){Promise.resolve().then(u.t.bind(u,65878,23))}},function(n){n.O(0,[5878,2971,2117,1744],function(){return n(n.s=33715)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/gerd-ama/page-d2eea7be33c4c221.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7479],{19518:function(e,n,s){Promise.resolve().then(s.t.bind(s,65878,23)),Promise.resolve().then(s.t.bind(s,72972,23))}},function(e){e.O(0,[5878,2972,2971,2117,1744],function(){return e(e.s=19518)}),_N_E=e.O()}]);
